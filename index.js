b712554286c37c3e6b8ae2b196389be2fa9709cd
HatemZreek88 <hatem.zreek88@gmail.com>
2019-08-12 15:52:03 +0200

    Variable Info!

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-let firstName = "Hatem";
-let lastName = "Zreek";
-console.log(firstName, lastName);
+// First Exercise
+    let firstName = "Hatem";
+    let lastName = "Zreek";
+    console.log(firstName, lastName);
+// Second Exercise
+    let name = "Natalia";
+    let Nationality = "Russia";
+    let favoriteColor = "green";
+    let Job = "JuniorProgrammer";
+    let age = 33;
+    console.log(name, Nationality, favoriteColor, Job, age);
