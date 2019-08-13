19d2dfa9a23bdc2a18923d2f2f73332cdd63f84e
HatemZreek88 <hatem.zreek88@gmail.com>
2019-08-13 10:01:11 +0200

    Make changes

--- index.js
+++ index.js
@@ -11,53 +11,61 @@
 
     let ageMark = "31" , ageJohn = 32;
 
-    let comp = true;
+    let ageTest = ageMark > ageJohn;
 
     let a = 3 , b = 4 , c = 5;
 
     let programmingLanguage = "JavaScript" , isFun = true;
 
     let test;
-    
+
     let calculation = [(2*4) / (4-2)] + [(6/3) * (5-2)];
 
 // Vars & console.log() 
    
     console.log(firstName, lastName);
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Variable Info!
     
     console.log(name, nationality, favoriteColor, job, age);
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Concatenation
 
     console.log("I am "+ name+ " and I am from "+nationality+" .I work now as a "+job+" .I am "+age+" years old and my favorite color is "+favoriteColor+".");
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Data Types
    
     console.log("length is a "+typeof length+" ,surname is a "+typeof surname+" ,car is a "+typeof car+" ,and isOpen is a "+typeof isOpen);
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Number Vars
     
     console.log("ageMark is a "+typeof ageMark+" , and ageJohn is a "+typeof ageJohn);
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Who's older?
-   
-    console.log("John is older than Mark: "+ comp);
+
+    console.log("John is older than Mark: "+ ageTest); //ask
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Compare and contrast
    
     console.log("Is a === b :"+ (a===b) + " Is b === c :"+ (b===c) + " Is a ===c :"+ (a===c));
-    console.log("The type of this check is --> "+ typeof((a===b)&&(b===c)&&(a===c)));
+    console.log("The type of this check is --> "+ typeof((a===b)&&(b===c)&&(a===c))); //ask
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // isFun?
    
     console.log(programmingLanguage + " is fun:" +isFun);
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // What type is it?
     
-    console.log(typeof(test));
-    console.log("the type of the test is undefined because we need to assign a value otherwise it will still undefined.");
+    console.log(typeof(test) + "---> the type of the test is undefined because we need to assign a value otherwise it will still undefined.");
+    console.log("---------------------------------------------------------------------------------------------------------");
 
 // Calculation!
    
