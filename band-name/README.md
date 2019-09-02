2f3a65ac48018dd29342dbbde9959c699b78d59b
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-02 20:46:21 +0200

    generateBandName

--- band-name/README.md
+++
@@ -1,2 +0,0 @@
-# pair
-function repo
--- band-name/index.js
+++ band-name/index.js
@@ -1,13 +1,10 @@
-function bandNameFunction(clothingColor, lastFoodEaten) {
-  const firstInitial = clothingColor[0];
-  const secondInitial = lastFoodEaten[0];
-  const firstInitialUpperCase = firstInitial.toUpperCase();
-  const secondInitialUpperCase = secondInitial.toUpperCase();
-  const bandName = `The ${firstInitialUpperCase}${clothingColor.substring(
-    1
-  )} ${secondInitialUpperCase}${lastFoodEaten.substring(1)}!`;
-
-  return bandName;
+function generateBandName(clothingColour, lastFoodEaten) {
+  const firstInitialUpperCase = clothingColour[0].toUpperCase();
+  const secondInitialUpperCase = lastFoodEaten[0].toUpperCase();
+  return `The ${firstInitialUpperCase}${clothingColour
+    .substring(1)
+    .toLowerCase()} ${secondInitialUpperCase}${lastFoodEaten
+    .substring(1)
+    .toLowerCase()}!`;
 }
-const globalArr = bandNameFunction("white", "chiliconcarne"); // store the function in a global array.
-console.log(globalArr);
+console.log(generateBandName("wHite", "chiliCONcarne"));
