40c7668f5f2d8a65a9c20350ffdd40f9b96529a2
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-02 21:05:41 +0200

    original edition

--- band-name/index.js
+++ band-name/index.js
@@ -1,11 +1,10 @@
 function generateBandName(clothingColour, lastFoodEaten) {
   const firstInitialUpperCase = clothingColour[0].toUpperCase();
   const secondInitialUpperCase = lastFoodEaten[0].toUpperCase();
-  const bandName = `The ${firstInitialUpperCase}${clothingColour
+  return `The ${firstInitialUpperCase}${clothingColour
     .substring(1)
     .toLowerCase()} ${secondInitialUpperCase}${lastFoodEaten
     .substring(1)
     .toLowerCase()}!`;
-  return bandName;
 }
 console.log(generateBandName("wHite", "chiliCONcarne"));
