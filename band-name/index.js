d65c91fa75d25a78772c427aff8c672f3624a916
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-02 20:55:30 +0200

    one return statement

--- band-name/index.js
+++ band-name/index.js
@@ -1,10 +1,11 @@
 function generateBandName(clothingColour, lastFoodEaten) {
   const firstInitialUpperCase = clothingColour[0].toUpperCase();
   const secondInitialUpperCase = lastFoodEaten[0].toUpperCase();
-  return `The ${firstInitialUpperCase}${clothingColour
+  const bandName = `The ${firstInitialUpperCase}${clothingColour
     .substring(1)
     .toLowerCase()} ${secondInitialUpperCase}${lastFoodEaten
     .substring(1)
     .toLowerCase()}!`;
+  return bandName;
 }
 console.log(generateBandName("wHite", "chiliCONcarne"));
