aa07d8689a85e2a8146a161bb32daea28e9c8d93
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-03 14:27:22 +0200

    updating global arr

--- band-name/index.js
+++ band-name/index.js
@@ -1,11 +1,17 @@
+const globalArr = [];
+
 function generateBandName(clothingColour, lastFoodEaten) {
+  let bandName = "";
   const firstInitialUpperCase = clothingColour[0].toUpperCase();
   const secondInitialUpperCase = lastFoodEaten[0].toUpperCase();
-  return `The ${firstInitialUpperCase}${clothingColour
+  bandName = `The ${firstInitialUpperCase}${clothingColour
     .substring(1)
     .toLowerCase()} ${secondInitialUpperCase}${lastFoodEaten
     .substring(1)
     .toLowerCase()}!`;
+  globalArr.push(bandName);
+  //console.log(globalArr);
+  return bandName;
 }
-const globalArr = generateBandName("wHite", "chiliCONcarne");
-console.log(globalArr);
+
+console.log(generateBandName("whitE", "burger"));
