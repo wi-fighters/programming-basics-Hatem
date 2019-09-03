a16cf500d5e223d4a6cd7c2a550962f4f49332a8
Hatem Zreek <53761003+HatemZreek88@users.noreply.github.com>
2019-09-03 15:03:34 +0200

    Update index.js

--- band-name/index.js
+++ band-name/index.js
@@ -37,30 +37,3 @@ function makeTasty(color) {
 console.log(generateBandName("black", "spAGhetti"));
 
 
-const globalArr = [];
-
-function generateBandName(clothingColour, lastFoodEaten) {
-  let bandName = "";
-  const firstInitialUpperCase = clothingColour[0].toUpperCase();
-  const secondInitialUpperCase = lastFoodEaten[0].toUpperCase();
-  bandName = `The ${firstInitialUpperCase}${clothingColour
-    .substring(1)
-    .toLowerCase()} ${secondInitialUpperCase}${lastFoodEaten
-    .substring(1)
-    .toLowerCase()}!`;
-  globalArr.push(bandName);
-
-  return bandName;
-}
-
-console.log(generateBandName("whitE", "burger"));
-
-function capitalizeInitial(rawString) {
-  const firstInitialUpperCase = rawString[0].toUpperCase();
-  const bandName = `${firstInitialUpperCase}${rawString
-    .substring(1)
-    .toLowerCase()}`;
-  return bandName;
-}
-console.log(capitalizeInitial("WhIte"), capitalizeInitial("ChiliConcarne"));
-
