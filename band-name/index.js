ba47640940492e4e1f3a982f2d82652d8159c29f
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-03 14:53:11 +0200

    makeTasty func

--- band-name/index.js
+++ band-name/index.js
@@ -1,17 +1,36 @@
-const globalArr = [];
-
-function generateBandName(clothingColour, lastFoodEaten) {
+function generateBandName(clothingColor, lastFoodEaten) {
   let bandName = "";
-  const firstInitialUpperCase = clothingColour[0].toUpperCase();
-  const secondInitialUpperCase = lastFoodEaten[0].toUpperCase();
-  bandName = `The ${firstInitialUpperCase}${clothingColour
-    .substring(1)
-    .toLowerCase()} ${secondInitialUpperCase}${lastFoodEaten
-    .substring(1)
-    .toLowerCase()}!`;
-  globalArr.push(bandName);
-  //console.log(globalArr);
+  const colorInitial = clothingColor[0].toUpperCase();
+  const colorRest = clothingColor.substr(1).toLowerCase();
+  const color = colorInitial.concat(colorRest);
+  const foodInitial = lastFoodEaten[0].toUpperCase();
+  const foodRest = lastFoodEaten.substr(1).toLowerCase();
+  const food = foodInitial.concat(foodRest);
+  bandName = `The ${makeTasty(color)} ${food}!`;
   return bandName;
 }
-
-console.log(generateBandName("whitE", "burger"));
+function makeTasty(color) {
+  switch (color) {
+    case "Red":
+      return "Chilli";
+      break;
+    case "Orange":
+      return "Tangerine";
+      break;
+    case "Yellow":
+      return "Pineapple";
+      break;
+    case "Green":
+      return "Sage";
+      break;
+    case "Blue":
+      return "Blueberry";
+      break;
+    case "Purple":
+      return "Plum";
+      break;
+    default:
+      return color;
+  }
+}
+console.log(generateBandName("black", "spAGhetti"));
