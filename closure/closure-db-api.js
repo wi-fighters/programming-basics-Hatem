ef2d1a2001a25575fbdc99c93e3bd9617fe3f321
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-12 13:57:39 +0200

    add read

--- closure/closure-db-api.js
+++ closure/closure-db-api.js
@@ -3,17 +3,22 @@ const init = () => {
   let api = [];
   const create = element => {
     db.push(element);
-    console.log("contents of db:");
-    console.log(db);
+    // console.log("contents of db:");
+    // console.log(db);
     return db.length;
   };
-  api.push(create);
-  console.log("contents of api:");
-  console.log(api);
+  const read = () => {
+    let copiedArray = [...db];
+    return copiedArray;
+  };
+  api.push(create, read);
+  //   console.log("contents of api:");
+  //   console.log(api);
   return api;
 };
 const createInDb = init();
-const myElement = createInDb[0];
-console.log(myElement("Hatem"));
+console.log(createInDb[0]("Hatem"));
 console.log(createInDb[0]("Lubna"));
 console.log(createInDb[0]("Adeeb"));
+console.log(createInDb[1]());
+console.log(createInDb[1]("new value")); // trying to push element to the db from outside the init function and i can not because i am just reading the array not to change with it.
