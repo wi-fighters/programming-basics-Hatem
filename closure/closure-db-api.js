d682603e3c6a2ee8d51d327d21516b1e1f740cfe
HatemZreek88 <hatem.zreek88@gmail.com>
2019-09-13 10:12:34 +0200

    Delete Phase

--- closure/closure-db-api.js
+++ closure/closure-db-api.js
@@ -3,24 +3,36 @@ const init = () => {
   let api = [];
   const create = element => {
     db.push(element);
-    // console.log("contents of db:");
-    // console.log(db);
-    return `New element added! The new length of the array is ${db.length}.`;
+    return `Create Phase: \n New element added! The new length of the array is ${db.length}. \n -------------------------`;
   };
   const read = () => {
     let copiedArray = [...db];
-    return copiedArray;
+    return ` Read Phase: \n ${copiedArray} \n ----------------------`;
   };
   const update = (index, newElement) => {
     if (db[index]) {
+      console.log(
+        `Update Phase: \n This index has an element \n now I will update the array\n---------------`
+      );
       return (db[index] = newElement);
     } else {
+      console.log(
+        `Update Phase: \n This index is new \n now I will create new element \n-------------`
+      );
       return create(newElement);
     }
   };
-  api.push(create, read, update);
-  //   console.log("contents of api:");
-  //   console.log(api);
+  const remove = index => {
+    if (db[index]) {
+      console.log(
+        `Delete Phase: \n I want to delete the element inside this index: ${index}`
+      );
+      return db.splice(index, 1);
+    } else {
+      return "There is no index to delete the element inside it.";
+    }
+  };
+  api.push(create, read, update, remove);
   return api;
 };
 const createInDb = init();
@@ -28,8 +40,13 @@ console.log(createInDb[0]("Hatem"));
 console.log(createInDb[0]("Lubna"));
 console.log(createInDb[0]("Adeeb"));
 console.log(createInDb[1]());
-console.log(createInDb[1]("new value")); // trying to push element to the db from outside the init function and i can not because i am just reading the array not to change with it.
-console.log(createInDb[2](0, "hi i am a new element"));
+console.log(
+  "I can't update the array because I am in the read phase \n" +
+    createInDb[1]("new element")
+);
+console.log(createInDb[2](0, "Adam"));
 console.log(createInDb[1]());
-console.log(createInDb[2](11, "hi i am a new element"));
+console.log(createInDb[2](3, "Hatem"));
 console.log(createInDb[1]());
+console.log(createInDb[3](0));
+console.log("-------------\n" + createInDb[1]());
